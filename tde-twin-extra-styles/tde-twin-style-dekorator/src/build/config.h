76702a9107e75d0f2225424bd15c764090627fe8
Martin Fritz <Fritz.marin99@web.de>
2022-05-02 14:56:38 +0200

    removed build files from dekorator

--- tde-twin-extra-styles/tde-twin-style-dekorator/src/build/config.h
+++
@@ -1,8 +0,0 @@
-#define VERSION "R14.0.12"
-
-// Defined if you have fvisibility and fvisibility-inlines-hidden support.
-#define __KDE_HAVE_GCC_VISIBILITY 1
-
-/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
-   significant byte first (like Motorola and SPARC, unlike Intel). */
-/* #undef WORDS_BIGENDIAN */
